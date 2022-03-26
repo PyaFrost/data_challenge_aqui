7e382a32c093668b205eadd730a682e36d3c0524
eFrost <89226202+PyaFrost@users.noreply.github.com>
2022-03-26 21:50:57 +0100

    Create params_data.py

---
+++ Sources/params_data.py
@@ -0,0 +1,164 @@
+params = {'client' : 
+              {'col_to_del' : ['un site internet', 'nom', 'prénom', "siret de l'entreprise"],
+               
+               'dict_ville' : 
+                   {'Métier du poste' : 
+                        {'false' : ['AGENT DENTRETIEN', 'ANIMATRICE (TEUR)', 'ASSITANTE COMMERCIALE', 'CARISTE-ROULEUR', 
+                                    'CHARGEE DE CLIENTELE', 'CONDITIONNEUSE', 'CONDITONNEUSE', 'EDUCATRICE SPORTIVE',
+                                    'ELECRTICIEN INDUSTRIEL', 'HOTESSE DACCUEIL', 'SERVEUSE', 'VENDEUR(SE)', 'VENDEUSE', 
+                                    'CONDITIONNEUSE CERCLEUSE', 'CONDUCTEUR DENGINS', 'TELECONSEILLERE'],
+                        
+                         'true' :  ["AGENT D'ENTRETIEN", 'ANIMATEUR', 'ASSISTANT COMMERCIAL', 'CARISTE ROULEUR', 
+                                    'CHARGE DE CLIENTELE', 'CONDITIONNEUR', 'CONDITIONNEUR', 'EDUCATEUR SPORTIF',
+                                    'ELECTRICIEN INDUSTRIEL',  "HOTESSE D'ACCUEIL", 'SERVEUR', 'VENDEUR', 'VENDEUR', 
+                                    'CONDITIONNEUR CERCLEUR', "CONDUCTEUR D'ENGINS", 'TELECONSEILLER']
+                        },
+                    
+                    'Localisation du poste' :
+                        {'false' : ['AIRE SUR LADOUR', 'ARTHEZ DE BEARN', 'CASTEIDE CAMI', 'CASTEL-SARRAZIN', 'LABASTIDE CEZERACQ',
+                                    'LABASTIDE MONREJEAU', 'LENDRESE', 'OLORON STE MARIE', 'OS MARSILLON', 'POEY DE LESCAR',
+                                    'SALIES DE BEARN', 'SALLES MONGISCARD', 'SERRES STE MARIE', 'ST AGNET', 'ST BOES'],
+                    
+                         'true' :  ['AIRE-SUR-LADOUR', 'ARTHEZ-DE-BEARN', 'CASTEIDE-CAMI', 'CASTELSARRASIN', 'LABASTIDE-CEZERACQ', 
+                                    'LABASTIDE-MONREJEAU', 'LENDRESSE', 'OLORON-SAINTE-MARIE', 'OS-MARSILLON', 'POEY-DE-LESCAR', 
+                                    'SALIES-DE-BEARN', 'SALLES-MONGISCARD', 'SERRES-SAINTE-MARIE', 'SAINT-AGNET', 'SAINT-BOES']
+                        }
+                   }
+              },
+          
+          'jobbeur' : 
+              {'col_to_del' : ['Permis PL', 'Caces 1', 'Caces 2', 'Caces 3', 'Autre transports',
+                               'Si vous avez un profil Linkedin ou un e-CV, coller le lien URL :', 
+                               'Si vous avez un site internet, blog ou portfolio coller le lien URL', 
+                               'NOM', 'Prénom', 'Quel type de contrat vous intéresse 1', '0- 20klm',
+                               'Localisation géographique du poste (région, ville)'],
+               
+               'dict_ville' : 
+                   {'VILLE' :
+                        {'false' : ['AIRE SUR LADOUR', 'ARTHEZ DE BEARN', 'CASTEIDE CAMI', 'CASTEL-SARRAZIN', 'LABASTIDE CEZERACQ',
+                                    'LABASTIDE MONREJEAU', 'LENDRESE', 'OLORON STE MARIE', 'OS MARSILLON', 'POEY DE LESCAR',
+                                    'SALIES DE BEARN', 'SALLES MONGISCARD', 'SERRES STE MARIE', 'ST AGNET', 'ST BOES', 'STE COLOME'],
+                    
+                         'true' :  ['AIRE-SUR-LADOUR', 'ARTHEZ-DE-BEARN', 'CASTEIDE-CAMI', 'CASTELSARRASIN', 'LABASTIDE-CEZERACQ', 
+                                    'LABASTIDE-MONREJEAU', 'LENDRESSE', 'OLORON-SAINTE-MARIE', 'OS-MARSILLON', 'POEY-DE-LESCAR', 
+                                    'SALIES-DE-BEARN', 'SALLES-MONGISCARD', 'SERRES-SAINTE-MARIE', 'SAINT-AGNET', 'SAINT-BOES', 
+                                    'SAINTE-COLOME']
+                        },
+                    
+                    'Dernier poste occupé (ou actuel)' :
+                        {'false' : ['AGENT D EXPLOITATION','AGENT DENTRETIEN', 'AGENT DEXPLOITATION', 'ANIMATRICE (TEUR)',
+                                    'ASSITANTE COMMERCIALE', 'CHARGEE DE CLIENTELE', 'CONDITIONNEUSE',
+                                    'CONDITIONNEUSE CERCLEUSE', 'CONDITONNEUSE', 'CONDUCTEUR DENGINS', 'EDUCATRICE SPORTIF', 
+                                    'EDUCATRICE SPORTIVE', 'ELECRTICIEN INDUSTRIEL', 'EMPLOYE(E) DE POSTE', 'HOTESSE DACCUEIL',
+                                    'MONTEUR ECHAFAUDAGE', 'MONTEUR ECHAFFAUDEUR', 'OUVRIER AGRO ALIMENTAIRE', 'PLONGEUR(SE)', 
+                                    'SERVEUSE', 'TELECONSEILLERE', 'VENDEUR(SE)', 'VENDEUSE'],
+                    
+                         'true' :  ["AGENT D'EXPLOITATION", "AGENT D'ENTRETIEN", "AGENT D'EXPLOITATION", 'ANIMATEUR',
+                                    'ASSISTANT COMMERCIAL', 'CHARGE DE CLIENTELE', 'CONDITIONNEUR',
+                                    'CONDITIONNEUR CERCLEUR', 'CONDITIONNEUR', 'CONDUCTEUR ENGINS', 'EDUCATEUR SPORTIF', 
+                                    'EDUCATEUR SPORTIF', 'ELECTRICIEN INDUSTRIEL', 'EMPLOYE DE POSTE', "HOTESSE D'ACCUEIL", 
+                                    'MONTEUR ECHAFFAUDAGE', 'MONTEUR ECHAFFAUDAGE', 'OUVRIER AGRO ALIMENTAIRE', 'PLONGEUR', 
+                                    'SERVEUR', 'TELECONSEILLER', 'VENDEUR', 'VENDEUR']
+                        },
+                    
+                    'Mission recherchée : Exemple n°1 de poste (métier + secteur)':
+                        {'false' : ['AGENT D EXPLOITATION', 'AGENT DENTRETIEN', 'AGENT DEXPLOITATION', 'ANIMATRICE (TEUR)',
+                                    'ASSITANTE COMMERCIALE', 'CHARGEE DE CLIENTELE', 'CONDITIONNEUSE', 'CONDITIONNEUSE CERCLEUSE',
+                                    'CONDITIONNEUSE CERCLEUSE', 'CONDUCTEUR DENGINS', 'EDUCATRICE SPORTIF', 'EDUCATRICE SPORTIF',
+                                    'EMPLOYE(E) DE POSTE', 'HOTESSE DACCUEIL', 'MONTEUR ECHAFAUDAGE', 'MONTEUR ECHAFFAUDEUR',
+                                    'OUVRIER AGRO ALIMENTAIRE',  'PLONGEUR(SE)', 'SERVEUSE', 'TELECONSEILLERE', 'VENDEUR(SE)', 'VENDEUSE'],
+                    
+                         'true'  : ["AGENT D'EXPLOITATION", "AGENT D'ENTRETIEN", "AGENT D'EXPLOITATION", 'ANIMATEUR',
+                                    'ASSISTANT COMMERCIAL', 'CHARGE DE CLIENTELE', 'CONDITIONNEUR', 'CONDITIONNEUR CERCLEUR',
+                                    'CONDITIONNEUR CERCLEUSE', 'CONDUCTEUR ENGINS', 'EDUCATEUR SPORTIF', 'EDUCATEUR SPORTIF',
+                                    'EMPLOYE DE POSTE', "HOTESSE D'ACCUEIL", 'MONTEUR ECHAFFAUDAGE', 'MONTEUR ECHAFFAUDAGE',
+                                    'OUVRIER AGROALIMENTAIRE',  'PLONGEUR', 'SERVEUR', 'TELECONSEILLER', 'VENDEUR', 'VENDEUR']
+                        }
+                   }
+              }
+         }
+
+paires = {
+    
+    'p1' : {
+        'item' : ['Localisation du poste', 'VILLE'],
+        'correction?' : 0,
+    },
+    
+    'p2' : {
+        'item' : ['Dernier poste occupé (ou actuel)', 'Métier du poste'],
+        'correction?' : 0,
+    },
+    
+    'p3' : {
+        'item' : ['Mission recherchée : Exemple n°1 de poste (métier + secteur)', 'Métier du poste'],
+        'correction?' : 0,
+    },
+    
+    'p4' : {
+        'item' : ["Quelle taille d'entreprise / organisation TEXTE", 'Taille entreprise'],
+        'correction?' : 1,
+        'corr' : [['ETI', 'ETI (50 à 500)'], 
+                  ['GRAND GROUPE',  'Grand Groupe (plus de 500 personnes)'],
+                  ['PME', 'PME (10 à 50)'], 
+                  ['TPE', 'TPE (1 à 10)']]
+    },
+    
+    'p5' : {
+        'item' : ['Voitures', 'Mobilité'],
+        'correction?' : 1,
+        'corr' : [['Véhiculé avec voiture', 'Voiture'], 
+                  ['Deux roues à moteur',  'Véhicule 2 roues'],
+                  ['Pas de véhicule', 'Transports en commun']]
+    },
+    
+    'p6' : {
+        'item' : ['Permis B', 'Mobilité'],
+        'correction?' : 1,
+        'corr' : [['Oui', 'Permis de conduire']]
+    },
+    
+    'p7' : {
+        'item' : ['Vos compétences 1', 'Comptétences pour le poste 1'],
+        'correction?' : 1,
+        'corr' : [['Motivation', 'Motivation, donner du sens au delà de la tâche à effectuer'],
+                  ['Vision', "Vision, voir le chemin pour arriver à l'objectif (dézoom)"]]
+    },
+    
+    'p8' : {
+        'item' : ['Vos compétences 2', 'Comptétences pour le poste 2'],
+        'correction?' : 1,
+        'corr' : [['Motivation', 'Motivation, donner du sens au delà de la tâche à effectuer'],
+                  ['Vision', "Vision, voir le chemin pour arriver à l'objectif (dézoom)"]]
+    },
+    
+    'p9' : {
+        'item' : ['Vos compétences 3', 'Comptétences pour le poste 3'],
+        'correction?' : 1,
+        'corr' : [['Motivation', 'Motivation, donner du sens au delà de la tâche à effectuer'],
+                  ['Vision', "Vision, voir le chemin pour arriver à l'objectif (dézoom)"]]
+    },
+    
+    'p10' : {
+        'item' : ['Quel type de contrat vous intéresse 2', 'Quels types de contrats pour les embauchés - TEXTE'],
+        'correction?' : 1,
+        'corr' : [['alternance, Appentissage', 'Alternance-Appentissage']]
+    },
+    
+    'p11' : {
+        'item' : ['Quel type de contrat vous intéresse 3', 'Quels types de contrats pour les embauchés - TEXTE'],
+        'correction?' : 1,
+        'corr' : [['alternance, Appentissage', 'Alternance-Appentissage']]
+    },
+    
+    'p12' : {
+        'item' : ['Vous souhaitez faire du télétravail (en % temps)', 'Télétravail ( en %)'],
+        'correction?' : 0
+    },
+    
+    'p12' : {
+        'item' : ['Vous êtes prêt à faire des déplacements professionnels (en % temps)',
+                  'Poste avec du déplacement (en %) si 75 ramené a 100%'],
+        'correction?' : 0
+    }
+}
